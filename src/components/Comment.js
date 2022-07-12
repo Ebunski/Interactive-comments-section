79490d4605c5b21c07fdb8368ba65f740674c08b
Catalyst <danielduyilemi@gmail.com>
2022-07-12 07:20:29 +0100

    Correct the path to the avatar images.

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -33,6 +33,9 @@ export default function Comment(props) {
 		parentId = null, //used to place the replies
 	} = props;
 
+	// Added this path to the front all images.... :) :) :)
+	const PUBLIC_URL = process.env.PUBLIC_URL + '/';
+
 	const fiveMinutes = 300000;
 	const timeElapsed = new Date() - new Date(createdAt) > fiveMinutes;
 	const authentication = user.username === currUser.username;
@@ -65,13 +68,12 @@ Comment component
 			<article className="comment">
 				<div className="likes">
 					<button className="plus" onClick={() => vote(id, 'plus')}>
-						<img s></img>
-						{/* <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+						<svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
 							<path
 								d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
 								fill="#C5C6EF"
 							/>
-						</svg> */}
+						</svg>
 					</button>
 
 					<p className="num">{score}</p>
@@ -87,17 +89,11 @@ Comment component
 				</div>
 
 				<div className="identity">
-					<img className="avatar" src={user.image.png} alt={user.username} />
-					<div className="name">{user.username}</div>
-					{/* ---style this----(displays if its current user)*/}
-					{authentication && <button>you</button>}
-				</div>
-				<div className="time">
-					<TimeAgo date={createdAt} formatter={formatter} />
-				</div>
-
-				<div className="identity">
-					<img className="avatar" src={user.image.png} alt={user.username} />
+					<img
+						className="avatar"
+						src={PUBLIC_URL + user.image.png}
+						alt={user.username}
+					/>
 					<div className="name">{user.username}</div>
 					{/* ---style this----(displays if its current user)*/}
 					{authentication && <button>you</button>}
--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -1,49 +1,52 @@
-import React, { useState } from "react";
-import { useGlobalContext } from "../context";
+import React, { useState } from 'react';
+import { useGlobalContext } from '../context';
 // import timeSince from "../timeTracker";
 
 export default function Markdown(props) {
-  const { currUser, currId, actionType, handleAdd, handleEdit, changeAction } =
-    useGlobalContext();
-  const { label, replyId = null, initialText = "" } = props;
-  const [text, setText] = useState(initialText);
+	const { currUser, currId, actionType, handleAdd, handleEdit, changeAction } =
+		useGlobalContext();
+	const { label, replyId = null, initialText = '' } = props;
+	const [text, setText] = useState(initialText);
 
-  /*----------------------------------FUNCTIONS--------------------------------*/
+	// Added this path to the front all images.... :) :) :)
+	const PUBLIC_URL = process.env.PUBLIC_URL + '/';
 
-  const isDisabled = text.length === 0; //***** *//
+	/*----------------------------------FUNCTIONS--------------------------------*/
 
-  function handleSubmit(event) {
-    event.preventDefault();
+	const isDisabled = text.length === 0; //***** *//
 
-    if (actionType === "edit") {
-      handleEdit(currId, text);
-      changeAction(null);
-    } else {
-      const input = {
-        id: Math.random().toString(36).substring(2, 9), // *********//
-        content: text,
-        createdAt: new Date(), //timeSince(new Date(Date.now()))
-        score: 1,
+	function handleSubmit(event) {
+		event.preventDefault();
 
-        replyingTo: "",
-        user: currUser, // never pass userId to API
-        replies: [],
-      };
+		if (actionType === 'edit') {
+			handleEdit(currId, text);
+			changeAction(null);
+		} else {
+			const input = {
+				id: Math.random().toString(36).substring(2, 9), // *********//
+				content: text,
+				createdAt: new Date(), //timeSince(new Date(Date.now()))
+				score: 1,
 
-      handleAdd(input, replyId);
-      setText("");
-    }
-  }
+				replyingTo: '',
+				user: currUser, // never pass userId to API
+				replies: [],
+			};
 
-  /* 
+			handleAdd(input, replyId);
+			setText('');
+		}
+	}
+
+	/* 
     ============
       MARKDOWN
     =============
 
   */
-  return (
-    <div>
-      {/* 
+	return (
+		<div>
+			{/* 
       ============
       You would create two classes for the form:
       1) For the normal markdown with grid - 3 columns
@@ -53,34 +56,37 @@ export default function Markdown(props) {
       =============
       */}
 
-      <form
-        className={initialText !== "" ? `edit-form` : `add-my-comment`}
-        //because initialText is empty in commenting state
-        onSubmit={handleSubmit}
-      >
-        {/* User image not displayed in edit state */}
-        {initialText === "" && (
-          <div className="img">
-            <img src={currUser.image.webp} alt={currUser.username} />
-          </div>
-        )}
+			<form
+				className={initialText !== '' ? `edit-form` : `add-my-comment`}
+				//because initialText is empty in commenting state
+				onSubmit={handleSubmit}
+			>
+				{/* User image not displayed in edit state */}
+				{initialText === '' && (
+					<div className="img">
+						<img
+							src={PUBLIC_URL + currUser.image.webp}
+							alt={currUser.username}
+						/>
+					</div>
+				)}
 
-        {/* changed to textarea */}
-        <textarea
-          type="text"
-          placeholder="Add a comment"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-        />
-        {initialText === "" && <button disabled={isDisabled}>{label}</button>}
+				{/* changed to textarea */}
+				<textarea
+					type="text"
+					placeholder="Add a comment"
+					value={text}
+					onChange={(e) => setText(e.target.value)}
+				/>
+				{initialText === '' && <button disabled={isDisabled}>{label}</button>}
 
-        {/*please style this div such that the button is on the right (click edit to view)*/}
-        {initialText !== "" && (
-          <div>
-            <button>{label}</button>
-          </div>
-        )}
-      </form>
-    </div>
-  );
+				{/*please style this div such that the button is on the right (click edit to view)*/}
+				{initialText !== '' && (
+					<div>
+						<button>{label}</button>
+					</div>
+				)}
+			</form>
+		</div>
+	);
 }
